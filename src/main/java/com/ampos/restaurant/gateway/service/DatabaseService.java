599cebc25f9a4dc9e57d6b64fe871f47e0b5c52e
Mittraparb Chanasakulniyom <mittraparb.chanasakulniyom@thomsonreuters.com>
2019-03-23 20:33:18 +0700

    Add neo4j database service

---
+++ src/main/java/com/ampos/restaurant/gateway/service/DatabaseService.java
@@ -0,0 +1,95 @@
+package com.ampos.restaurant.gateway.service;
+
+import com.ampos.restaurant.gateway.enumeration.MenuType;
+import com.ampos.restaurant.gateway.enumeration.NodeLabel;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Result;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+import org.neo4j.graphdb.schema.Schema;
+import org.neo4j.helpers.TransactionTemplate;
+import org.neo4j.kernel.configuration.BoltConnector;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+@Service
+public class DatabaseService {
+    private GraphDatabaseService graphDatabaseService;
+
+    public GraphDatabaseService getGraphDatabaseService() {
+        if (graphDatabaseService == null) {
+            BoltConnector bolt = new BoltConnector("0");
+            graphDatabaseService = new GraphDatabaseFactory()
+                    .newEmbeddedDatabaseBuilder(new File("cache.db"))
+                    .setConfig(GraphDatabaseSettings.pagecache_memory, "512M")
+                    .setConfig(GraphDatabaseSettings.string_block_size, "60")
+                    .setConfig(GraphDatabaseSettings.array_block_size, "300")
+                    .setConfig(bolt.type, "BOLT")
+                    .setConfig(bolt.enabled, "true")
+                    .setConfig(bolt.listen_address, "0.0.0.0:7687")
+                    .newGraphDatabase();
+
+            createNodeIndexes();
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    graphDatabaseService.shutdown();
+                }
+            });
+        }
+        return graphDatabaseService;
+    }
+
+    private void createNodeIndexes() {
+        try (Transaction tx = graphDatabaseService.beginTx()) {
+            Schema schema = graphDatabaseService.schema();
+            schema.indexFor(NodeLabel.User).on("uuid").create();
+            schema.indexFor(NodeLabel.Tag).on("name").create();
+            schema.indexFor(NodeLabel.Menu).on("name").create();
+            schema.indexFor(MenuType.Pizza).on("name").create();
+            schema.indexFor(MenuType.Beverage).on("name").create();
+            schema.indexFor(MenuType.Dumpling).on("name").create();
+            schema.indexFor(MenuType.Dessert).on("name").create();
+            schema.indexFor(MenuType.Noodle).on("name").create();
+            schema.indexFor(MenuType.Stew).on("name").create();
+            tx.success();
+        }
+    }
+
+    public <T> T[] execute(String query, Function<Result, T[]> parsingFunction) {
+        return new TransactionTemplate()
+                .retries(4)
+                .backoff(3, TimeUnit.SECONDS)
+                .with(graphDatabaseService)
+                .execute((Function<Transaction, T[]>) transaction -> parsingFunction.apply(graphDatabaseService.execute(query)));
+    }
+
+    public <T> T[] execute(String query, Map<String, Object> parameters, Function<Result, T[]> parsingFunction) {
+        return new TransactionTemplate()
+                .retries(4)
+                .backoff(3, TimeUnit.SECONDS)
+                .with(graphDatabaseService)
+                .execute((Function<Transaction, T[]>) transaction -> parsingFunction.apply(graphDatabaseService.execute(query, parameters)));
+    }
+
+    public <T> T executeSingle(String query, Function<Result, T> parsingFunction) {
+        return new TransactionTemplate()
+                .retries(4)
+                .backoff(3, TimeUnit.SECONDS)
+                .with(graphDatabaseService)
+                .execute((Function<Transaction, T>) transaction -> parsingFunction.apply(graphDatabaseService.execute(query)));
+    }
+
+    public <T> T executeSingle(String query, Map<String, Object> parameters, Function<Result, T> parsingFunction) {
+        return new TransactionTemplate()
+                .retries(4)
+                .backoff(3, TimeUnit.SECONDS)
+                .with(graphDatabaseService)
+                .execute((Function<Transaction, T>) transaction -> parsingFunction.apply(graphDatabaseService.execute(query, parameters)));
+    }
+}
